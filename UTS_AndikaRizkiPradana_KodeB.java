5d52a5a17d487c7b3126c801f90eb934bb846170
Andika Rizki Pradana <andikarizki01@gmail.com>
2019-12-04 08:42:26 +0700

    Update UTS_AndikaRizkiPradana_KodeB.java

--- UTS_AndikaRizkiPradana_KodeB.java
+++ UTS_AndikaRizkiPradana_KodeB.java
@@ -7,8 +7,8 @@ import javax.swing.JOptionPane;
 public class UTS_AndikaRizkiPradana_KodeB {
     public static void main(String[] args) {
         
-        int jmlBuku, konfirKeranjang, konfirPembelian, konfirMember;
-        double harga = 0, totalHarga = 0, estimasiDiskon, totalBelanjaMember;
+        int jmlBuku, konfirKeranjang, konfirPembelian, konfirMember, totalBuku = 0, diskonToko = 0;
+        double harga = 0, totalHarga = 0, estimasiDiskon, totalBelanjaMember, totalBelanja;
         String kodeBuku, inforBuku, checkout = "";
         
         jmlBuku = Integer.parseInt(JOptionPane.showInputDialog("Berapa jumlah buku yang ingin anda cari infonya?"));
@@ -27,37 +27,37 @@ public class UTS_AndikaRizkiPradana_KodeB {
             
             switch(kodeBuku){
                 case "181818A":
-                    inforBuku = "Judul Buku: Dhirga\nNama Pengarang: Natalia Tan";
+                    inforBuku = "Kode Buku: 181818A\nJudul Buku: Dhirga\nNama Pengarang: Natalia Tan";
                     harga = 68000; 
                     break;
                     
                 case "191919B":
-                    inforBuku = "Judul Buku: Summer Sky\nNama Pengarang: Stephanie Zen";
+                    inforBuku = "Kode Buku: 191919B\nJudul Buku: Summer Sky\nNama Pengarang: Stephanie Zen";
                     harga = 124000;
                     break; 
                     
                 case "202020C":
-                    inforBuku = "Judul Buku: Noversation\nNama Pengarang: Valerie Patkar";
+                    inforBuku = "Kode Buku: 202020C\nJudul Buku: Noversation\nNama Pengarang: Valerie Patkar";
                     harga = 115000;
                     break; 
                     
                 case "171717D":
-                    inforBuku = "Judul Buku: Si Anak Badai\nNama Pengarang: Tere Liye";
+                    inforBuku = "Kode Buku: 171717D\nJudul Buku: Si Anak Badai\nNama Pengarang: Tere Liye";
                     harga = 75000;
                     break; 
                     
                 case "161616E":
-                    inforBuku = "Judul Buku: Komet Minor\nNama Pengarang: Tere Liye";
+                    inforBuku = "Kode Buku: 161616E\nJudul Buku: Komet Minor\nNama Pengarang: Tere Liye";
                     harga = 71500;
                     break; 
                     
                 case "101010F":
-                    inforBuku = "Judul Buku: Me and My Broken Heart\nNama Pengarang: Wulafandi";
+                    inforBuku = "Kode Buku: 101010F\nJudul Buku: Me and My Broken Heart\nNama Pengarang: Wulafandi";
                     harga = 69500;
                     break; 
                     
                 case "090909G":
-                    inforBuku = "Judul Buku: Dilan\nNama Pengarang: Pidi Baiq";
+                    inforBuku = "Kode Buku: 090909G\nJudul Buku: Dilan\nNama Pengarang: Pidi Baiq";
                     harga = 59000;
                     break; 
                 
@@ -66,13 +66,12 @@ public class UTS_AndikaRizkiPradana_KodeB {
                     break;
             }
             
-            konfirKeranjang = Integer.parseInt(JOptionPane.showInputDialog("Informasi Buku, \n\n" +inforBuku+ "\nHarga: " +kursIndonesia.format(harga)+ "\n\nApakah ingin menambahkan buku ke keranjang belanja? (1.Ya / 2.Tidak)"));
+            konfirKeranjang = Integer.parseInt(JOptionPane.showInputDialog("Informasi Buku, \n\n" +inforBuku+ "\n\nHarga: " +kursIndonesia.format(harga)+ "\n\nApakah ingin menambahkan buku ke keranjang belanja? (1.Ya / 2.Tidak)"));
            
             if(konfirKeranjang == 1){
+                totalBuku++;
                 totalHarga += harga;
                 checkout += inforBuku+"\n\n ";
-            } else {
-                JOptionPane.showMessageDialog(null, "Terima Kasih Telah Mengunjungi Toko Kami");
             }
             
         } //End for
@@ -81,21 +80,39 @@ public class UTS_AndikaRizkiPradana_KodeB {
         konfirPembelian = Integer.parseInt(JOptionPane.showInputDialog(null, "Informasi Keranjang, \n\n\n" +checkout+ "\nTotal Harga: " +kursIndonesia.format(totalHarga)+ "\n\nApakah anda ingin membeli? (1.Ya / 2.Tidak)"));
         if (konfirPembelian == 1) {
             
-            konfirMember = Integer.parseInt(JOptionPane.showInputDialog(null, "Apakah anda mempunyai kartu member? (1.Ya / 2.Tidak)"));
-            if(konfirMember == 1){
-                estimasiDiskon = (float) (totalHarga * 0.15);
-                totalBelanjaMember = totalHarga - estimasiDiskon;
-                JOptionPane.showMessageDialog(null, "Informasi Pembelian, \n\n\n" +checkout+ "\nTotal Harga: " +kursIndonesia.format(totalHarga)+ "\n\nDiskon Member: 15%\nDiskon Toko: " +kursIndonesia.format(estimasiDiskon)+ "\n\nTotal Belanja: " +kursIndonesia.format(totalBelanjaMember));
-                JOptionPane.showMessageDialog(null, "Terima Kasih Telah Mengunjungi Toko Kami");
-                
-            } else {    
-                JOptionPane.showMessageDialog(null, "Informasi Pembelian, \n\n\n" +checkout+ "Total Belanja: " +kursIndonesia.format(totalHarga));
-                JOptionPane.showMessageDialog(null, "Terima Kasih Telah Mengunjungi Toko Kami");
+            if (totalBuku > 3) {
+                diskonToko = 7000;
+                konfirMember = Integer.parseInt(JOptionPane.showInputDialog(null, "Apakah anda mempunyai kartu member? (1.Ya / 2.Tidak)"));
+                if(konfirMember == 1){
+                    estimasiDiskon = (float) (totalHarga * 0.15);
+                    totalBelanjaMember = totalHarga - estimasiDiskon - diskonToko;
+                    JOptionPane.showMessageDialog(null, "Informasi Pembelian, \n\n\n" +checkout+ "\nTotal Harga: " +kursIndonesia.format(totalHarga)+ "\n\nDiskon Member: 15%\nDiskon Toko: " +kursIndonesia.format(diskonToko)+ "\n\nTotal Belanja: " +kursIndonesia.format(totalBelanjaMember));
+                    JOptionPane.showMessageDialog(null, "Terima Kasih Telah Mengunjungi Toko Kami");
+
+                } else {    
+                    totalBelanja = totalHarga - diskonToko;
+                    JOptionPane.showMessageDialog(null, "Informasi Pembelian, \n\n\n" +checkout+ "\nTotal Harga: " +kursIndonesia.format(totalHarga)+ "\n\nDiskon Toko: " +kursIndonesia.format(diskonToko)+ "\n\nTotal Belanja: " +kursIndonesia.format(totalBelanja));
+                    JOptionPane.showMessageDialog(null, "Terima Kasih Telah Mengunjungi Toko Kami");
+                }
+            } else {
+                diskonToko = 0;
+                konfirMember = Integer.parseInt(JOptionPane.showInputDialog(null, "Apakah anda mempunyai kartu member? (1.Ya / 2.Tidak)"));
+                if(konfirMember == 1){
+                    estimasiDiskon = (float) (totalHarga * 0.15);
+                    totalBelanjaMember = totalHarga - estimasiDiskon - diskonToko;
+                    JOptionPane.showMessageDialog(null, "Informasi Pembelian, \n\n\n" +checkout+ "\nTotal Harga: " +kursIndonesia.format(totalHarga)+ "\n\nDiskon Member: 15%\nDiskon Toko: " +kursIndonesia.format(diskonToko)+ "\n\nTotal Belanja: " +kursIndonesia.format(totalBelanjaMember));
+                    JOptionPane.showMessageDialog(null, "Terima Kasih Telah Mengunjungi Toko Kami");
+
+                } else {    
+                    totalBelanja = totalHarga - diskonToko;
+                    JOptionPane.showMessageDialog(null, "Informasi Pembelian, \n\n\n" +checkout+ "\nTotal Harga: " +kursIndonesia.format(totalHarga)+ "\n\nDiskon Toko: " +kursIndonesia.format(diskonToko)+ "\n\nTotal Belanja: " +kursIndonesia.format(totalBelanja));
+                    JOptionPane.showMessageDialog(null, "Terima Kasih Telah Mengunjungi Toko Kami");
+                }
             }
             
         } else {
             JOptionPane.showMessageDialog(null, "Terima Kasih Telah Mengunjungi Toko Kami");
-        } 
+        }
         
     }
 }
